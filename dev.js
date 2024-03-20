aac5d0ed499369e18d11ed831e01b9e3c40e3d60
parth7802 <ppatel7802@gmail.com>
2024-03-20 16:55:58 +0530

    Create dev.js

---
+++ dev.js
@@ -0,0 +1,18 @@
+<html> 
+
+     <head> 
+        <title>Welcome to servername.com!</title> 
+    </head> 
+ 
+    <body> 
+        <h1> 
+
+Success! The servername.com server block is working successfully! 
+
+        </h1> 
+        
+ <b>Meow Meow!</b> 
+ 
+    </body> 
+
+ </html>
